58fb8cf8bb3cd5898686a972f07eef9455f833cf
BriOD <briodonoghue@gmail.com>
2017-07-05 13:51:19 -0400

    build show action in menu-item controller

--- app/controllers/api/menu_items_controller.rb
+++ app/controllers/api/menu_items_controller.rb
@@ -4,6 +4,19 @@ class Api::MenuItemsController < ApplicationController
     render json: @menu_items.to_json
   end
 
+  def show
+    render json: MenuItem.find_by(id: params[:id]).to_json
+  end
+
+  def create
+    menu_item = MenuItem.create(menu_item_params)
+    if menu_item.save
+      render json: {id: menu_item.id, success: 'ok'}
+    else
+      render json: {message: 'error'}, status: 412
+    end
+  end
+
   private
     def menu_item_params
       params.require(:menu_iems).permit(:name, :description, :price)
